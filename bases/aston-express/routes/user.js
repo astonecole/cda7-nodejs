c272300989553b9998388820e5bf066930faa239
Vincent Letourneau <vincent@nanoninja.com>
2019-07-02 12:57:33 +0200

    Add routes

--- bases/aston-express/routes/user.js
+++ bases/aston-express/routes/user.js
@@ -1,6 +1,12 @@
 // routes/user.js
 
-// http://localhost:8000/lunchtime/user/register
+const controllers = require('../controllers/user');
 
 user = express.Router();
 lunchtime.use('/user', user);
+
+// http://localhost:8000/lunchtime/login
+lunchtime.post('/login', controllers.login);
+
+// http://localhost:8000/lunchtime/user/register
+user.post('/register', controllers.register);
