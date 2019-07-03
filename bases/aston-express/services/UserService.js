120af1ba58993c5443c1dd498a3378576512d944
Vincent Letourneau <vincent@nanoninja.com>
2019-07-03 12:12:09 +0200

    Add User service

--- bases/aston-express/services/UserService.js
+++ bases/aston-express/services/UserService.js
@@ -1,5 +1,18 @@
 // services/UserService.js
 
 const UserModel = require('../models/UserModel');
+const password = require('./utils/bcrypt-password');
+
+exports.create = user => {
+    // h = pass.hashSync('1234');
+    // console.log(pass.verifySync('12345', h));
+
+    return UserModel.create({
+        firstname: user.firstname,
+        email: user.email,
+        name: user.name,
+        password: user.password
+    });
+};
 
 exports.all = () => UserModel.findAll();
